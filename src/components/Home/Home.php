41cac9b6630088b71ba37f88328e3ff85d957fe0
CaptSiro <honzahorak433@icloud.com>
2026-04-27 12:07:55 +0200

    Implemented custom homepage

--- src/components/Home/Home.php
+++ src/components/Home/Home.php
@@ -9,6 +9,9 @@ use components\docs\Docs;
 use core\App;
 use core\RouteChasmEnvironment;
 use core\view\ContainerContent;
+use models\core\Page\Page;
+use models\core\Setting\Setting;
+use const models\extensions\Editable\PROPERTY_EDITABLE;
 
 class Home extends ContainerContent {
     protected Menu $menu;
@@ -50,5 +53,30 @@ class Home extends ContainerContent {
     public function getProjectName(): ?string {
         return App::getEnvStatic()
             ->get(RouteChasmEnvironment::ENV_PROJECT) ?? 'RouteChasm';
+    }
+
+    public function getCategoryPage(): ?Page {
+        $pageId = Setting::fromName(
+            'savora:homepage-categories-page-id',
+            true,
+            1,
+            [PROPERTY_EDITABLE => true]
+        )->toInt();
+
+        return Page::fromId($pageId);
+    }
+
+    /**
+     * @return array<Page>
+     */
+    public function getFeaturedPages(): array {
+        $limit = Setting::fromName(
+            'savora:homepage-featured-pages-count',
+            true,
+            3,
+            [PROPERTY_EDITABLE => true]
+        )->toInt();
+
+        return Page::lastUpdated(limit: $limit);
     }
 }
